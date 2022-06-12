f1858a916ff868bacc700c93180d324241dc8be7
Luiz Martins <luizmartins.uemg@gmail.com>
2022-06-12 20:34:53 -0300

    Add name_registration_app

---
+++ name_registration/name_registration/register.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="name_registration.register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txt_nome" runat="server" Height="28px" Width="248px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btn_adc" runat="server" OnClick="btn_adc_Click" Text="Adcionar" Width="80px" />
+&nbsp;
+            <asp:Button ID="btn_pesquisa" runat="server" OnClick="btn_pesquisa_Click" Text="Pesquisar" Width="80px" />
+&nbsp;
+            <asp:Button ID="btn_excluir" runat="server" OnClick="btn_excluir_Click" Text="Excluir" Width="80px" />
+            <br />
+            <br />
+            <asp:ListBox ID="listBox_nomes" runat="server" Height="175px" SelectionMode="Multiple" Width="254px"></asp:ListBox>
+&nbsp;
+            <asp:Button ID="btn_excluir_mult" runat="server" OnClick="btn_excluir_mult_Click" Text="Excluir Múltiplos" Width="120px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ name_registration/name_registration/register.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace name_registration
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_adc_Click(object sender, EventArgs e)
+        {
+            if (txt_nome.Text != "")
+                if (listBox_nomes.Items.FindByText(txt_nome.Text) == null)
+                    listBox_nomes.Items.Add(txt_nome.Text);
+                else
+                    txt_nome.Text = "Já existe o nome :)";
+        }
+
+        protected void btn_excluir_Click(object sender, EventArgs e)
+        {
+            if (listBox_nomes.SelectedIndex != -1)
+                listBox_nomes.Items.Remove(listBox_nomes.SelectedItem);
+            
+        }
+
+        protected void btn_excluir_mult_Click(object sender, EventArgs e)
+        {
+            for(int i = listBox_nomes.Items.Count - 1; i >= 0; i--)
+            {
+                if (listBox_nomes.Items[i].Selected)
+                    listBox_nomes.Items.Remove(listBox_nomes.Items[i]);
+            }
+        }
+
+        protected void btn_pesquisa_Click(object sender, EventArgs e)
+        {   
+            for (int i = 0; i < listBox_nomes.Items.Count; i++)
+                listBox_nomes.Items [i].Selected = false;
+
+            if(txt_nome.Text != "")
+            {
+ 
+                foreach(ListItem item in listBox_nomes.Items)
+                {
+                    if (item.Text == txt_nome.Text)
+                    {
+                        item.Selected = true;
+                        break;
+                    } 
+                }
+            }
+        }
+    }
+}
